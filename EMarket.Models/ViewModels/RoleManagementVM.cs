5f8083ba393f6e6abf07068c174874c50d7d4d12
Usman <usman62111@gmail.com>
2025-11-26 03:47:03 +0500

    User Management

---
+++ EMarket.Models/ViewModels/RoleManagementVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EMarket.Models.ViewModels
+{
+    public class RoleManagementVM
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<SelectListItem> RoleList { get; set; }
+        public IEnumerable<SelectListItem> CompanyList { get; set; }
+    }
+}
--- EMarket.Web/Areas/Admin/Controllers/CompanyController.cs
+++ EMarket.Web/Areas/Admin/Controllers/CompanyController.cs
@@ -15,7 +15,6 @@ namespace EMarket.Web.Areas.Admin.Controllers
     public class CompanyController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        private readonly IWebHostEnvironment _webHostEnvironment;
         public CompanyController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;            
